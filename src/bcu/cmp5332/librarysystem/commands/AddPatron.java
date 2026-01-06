ff9ef804faaa13d6d9afd5b0b88b4f5d28ea9da2
DJ <DJ@192.168.0.22>
2026-01-06 20:33:20 +0000

    AddPatron Update

--- src/bcu/cmp5332/librarysystem/commands/AddPatron.java
+++ src/bcu/cmp5332/librarysystem/commands/AddPatron.java
@@ -1,6 +1,7 @@
 package bcu.cmp5332.librarysystem.commands;
 
 import bcu.cmp5332.librarysystem.main.LibraryException;
+import bcu.cmp5332.librarysystem.model.Patron;
 import bcu.cmp5332.librarysystem.model.Library;
 
 import java.time.LocalDate;
@@ -9,15 +10,25 @@ public class AddPatron implements Command {
 
     private final String name;
     private final String phone;
+    private final String email;
 
-    public AddPatron(String name, String phone) {
+    public AddPatron(String name, String phone, String email) {
         this.name = name;
         this.phone = phone;
+        this.email = email;
     }
 
     @Override
     public void execute(Library library, LocalDate currentDate) throws LibraryException {
         // TODO: implementation here
+    	int maxId = 0;
+    	if (library.getPatrons().size() > 0) {
+    		int lastIndex = library.getPatrons().size() - 1;
+            maxId = library.getPatrons().get(lastIndex).getId();
+    	}
+        Patron patron = new Patron(++maxId, name, phone, email);
+        library.addPatron(patron);
+        System.out.println("Patron #" + patron.getId() + " added.");
     }
 }
  
