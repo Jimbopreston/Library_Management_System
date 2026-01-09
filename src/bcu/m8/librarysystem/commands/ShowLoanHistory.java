0e11802976fcdf1116e6917d7b0fc1468dab8f0a
Jim Preston <jimbopreston86@gmail.com>
2026-01-09 13:27:34 +0000

    showloanhistory javadoc done

--- src/bcu/m8/librarysystem/commands/ShowLoanHistory.java
+++ src/bcu/m8/librarysystem/commands/ShowLoanHistory.java
@@ -8,13 +8,24 @@ import bcu.m8.librarysystem.model.Loan;
 import bcu.m8.librarysystem.model.Patron;
 
 import java.util.List;
-
+/**
+ * Shows a patrons loan history.
+ * It is an implementation of the Command interface and its execute and altersdata methods to show a patrons loan history.
+ * It is created by command parser when the "showloanhistory" command is given by the user.
+ * The class has a constructor to initialise the fields needed to execute the command.
+ */
 public class ShowLoanHistory implements Command {
 private int patronId; //takes patron id as field
 
 	public ShowLoanHistory(int patronId) {
 		this.patronId = patronId; //constructor
 	}
+	/**
+	 * the execute method generates the patron object from the patron id and then checks the size of its books list.
+	 * If the list has a size of 0, the method throws a message saying that the patron has no loan history
+	 * @exception throws LibraryException when patron has no loan history
+	 * @param Has parameters of library and currentDate.
+	 */
 	@Override
 	public void execute(Library library, LocalDate currentDate) throws LibraryException {
 		Patron patron = library.getPatronByID(patronId); //generation of patron object
